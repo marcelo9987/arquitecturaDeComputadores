3be7e60768a704d412e1081221a7203bf8c57fb1
Maquina Virtual Ubuntu--SSOO <alcachofas27@l.net>
2023-02-27 12:53:37 +0100

    cambios

--- practica1/programa.c
+++ practica1/programa.c
@@ -9,6 +9,7 @@
 #define L2 64
 
 
+
 double media(int R, int D, double *valores)
 {
     double Salida = 0;
@@ -23,18 +24,20 @@ double media(int R, int D, double *valores)
 
 //
 
-int main(int argc, char *argcv[])
+int main(int argc, char *argv[])
 {
-//        size_t D = 2;
-//        size_t R = 4;
-//        double* A = _mm_malloc(R*D*sizeof(double),64);
-//        A[0]=1;
-//        A[1]=2;
-//        A[2]=3;
-//        A[3]=4;
-//        for(int iter=0;iter<4;iter++)
-//        {
-//                printf("%lf\n",A[iter]);
-//        }
-//        _mm_free(A);
+    int L[]={0.5*S1,1.5*S1,0.5*S2,0.75*S2,2*S2,4*S2,8*S2};
+    int L[7] =
+    /*        size_t D = 2;
+        size_t R = 4;
+        double* A = _mm_malloc(R*D*sizeof(double),64);
+        A[0]=1;
+        A[1]=2;
+        A[2]=3;
+        A[3]=4;
+        for(int iter=0;iter<4;iter++)
+        {
+                printf("%lf\n",A[iter]);
+        }
+        _mm_free(A);*/
 }
