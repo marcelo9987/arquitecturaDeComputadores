b79201b9bdec6a4488a16ed6addca45390ff3665
Maquina Virtual Ubuntu--SSOO <alcachofas27@l.net>
2023-02-27 12:36:20 +0100

    cambios

--- practica1/programa.c
+++ practica1/programa.c
@@ -8,25 +8,33 @@
 #define S2 20480
 #define L2 64
 
-/*
- * Se pide crear un vector dinámico, sumar este y, medir los ciclos requeridos.
- *
- *
- *
- */
 
-int main(int argc,char* argcv[])
+double media(int R, int D, double *valores)
 {
-        size_t D = 2;
-        size_t R = 4;
-        double* A = _mm_malloc(R*D*sizeof(double),64);
-        A[0]=1;
-        A[1]=2;
-        A[2]=3;
-        A[3]=4;
-        for(int iter=0;iter<4;iter++)
-        {
-                printf("%lf\n",A[iter]);
-        }
-        _mm_free(A);
+    double Salida = 0;
+    for (int i = 0; i < (R - 1); i++)
+    {
+        Salida += valores[R * D];
+    }
+    
+    salida /= R;
+    return salida;
+}
+
+//
+
+int main(int argc, char *argcv[])
+{
+//        size_t D = 2;
+//        size_t R = 4;
+//        double* A = _mm_malloc(R*D*sizeof(double),64);
+//        A[0]=1;
+//        A[1]=2;
+//        A[2]=3;
+//        A[3]=4;
+//        for(int iter=0;iter<4;iter++)
+//        {
+//                printf("%lf\n",A[iter]);
+//        }
+//        _mm_free(A);
 }
