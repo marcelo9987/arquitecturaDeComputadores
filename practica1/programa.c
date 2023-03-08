41df69fe3ea8d0b9a38dc877096f081789c4c94d
Windows-pat <imarcelo8000@gmail.com>
2023-03-08 16:42:46 +0100

    He hecho más parte del código. No estoy seguro si los valores van en otro array o no, pero lo he hecho de la primera forma igualmente.
    Queda relativamente poco, ahora voy a enlazar el .c y lo subo en unos minutos.
    
    Además,
    He modificado el código del CMakeLists.txt para que funcione en windows

--- practica1/programa.c
+++ practica1/programa.c
@@ -4,21 +4,31 @@
 #include <time.h>
 #include <float.h>
 #include <math.h>
-
+//getconf -a --> comando utilizado para saber los tamaños de las caches
+/*
+ * 
+ */
 #define S1 768
 #define L1 64
 
 #define S2 20480
 #define L2 64
 
+
+double generarNumero(double min, double max)
+{
+    double rango = (max - min);
+    double div = RAND_MAX / rango;
+    int aleatorio;
+    while ((aleatorio = rand()) == RAND_MAX); // Así evito dar el valor máximo (Se ha pedido intervalo abierto por la derecha)
+    return min + (aleatorio / div);
+}
+
 void aleatorizarValores(double *array, const int tamanho_array)
 {
-    srand(time(NULL));
-    
     for (int i = 0; i < tamanho_array; i++)
     {
-        
-        array[i] = (double) rand() / RAND_MAX;
+        array[i] = generarNumero(1,2);
     }
 }
 
@@ -38,7 +48,8 @@ double media(int R, int D, const double *valores)
 
 int main(int argc, char *argv[])
 {
-//    int L[]={(int)0.5*S1,(int)1.5*S1,(int)0.5*S2,(int)0.75*S2,2*S2,4*S2,8*S2};
+//    -int -L[]={(int)0.5*S1,(int)1.5*S1,(int)0.5*S2,(int)0.75*S2,2*S2,4*S2,8*S2};
+
     int D = atoi(argv[1]);
     int L = atoi(argv[2]);
     int R = 0;
@@ -64,10 +75,28 @@ int main(int argc, char *argv[])
     {
         R = L;
     }
-    double *A =(double *)_mm_malloc((R-1) * D * sizeof(double),64);
-    aleatorizarValores(A,(R-1) * D);
-    int indice
     
+    srand((time(NULL)));
+    
+    int  *indice = (int *) malloc(R * sizeof(int)); //todo: LIBERAME!
+    for (int i_indice_asignacionVariableIndice =0; i_indice_asignacionVariableIndice < R; i_indice_asignacionVariableIndice++)
+    {
+        indice[R] = i_indice_asignacionVariableIndice * D;
+    }
+    
+    
+    double *i_valores =(double *)_mm_malloc((R - 1) * D * sizeof(double), 64);//todo: LIBERAME!
+    double *A =(double *)_mm_malloc((R-1) * D * sizeof(double),64);//todo: LIBERAME!
+    aleatorizarValores(i_valores, (R - 1) * D);
+    
+    for (int i_indiceParaElIndice=0; i_indiceParaElIndice<R;i_indiceParaElIndice++)//Menudo plot-twist
+    {
+        A[indice[i_indiceParaElIndice]] = media(R, D, i_valores);
+    }
+    
+    free(i_valores);
+    free(A);
+    free(indice);
     
     
 }
