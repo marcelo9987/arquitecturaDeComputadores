64ab694233844706bb45a86e77f3c574e042b98c
Windows-pat <imarcelo8000@gmail.com>
2023-03-13 13:30:31 +0100

    Update script_experimentos.sh

--- practica1/script_experimentos.sh
+++ practica1/script_experimentos.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Solicitamos un nodo con 64 cores y 256 GB de memoria durante 2 horas
-#SBATCH -n 1 -c 64 -t 02:00:00 --mem=256G
+SBATCH -n 1 -c 64 -t 02:00:00 --mem=256G
 # Ponemos nombre a nuestro trabajo para poder identificarlo.
 # ATENCIÓN - Debes sustituir el NN por el número de equipo.
-#SBATCH --job-name p1acgNN
+SBATCH --job-name p1acg03
 
 # Sustituir los valores de Di y Li por los calculados para la realización de la práctica.
 
