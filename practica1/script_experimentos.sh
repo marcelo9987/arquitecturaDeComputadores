32f61b82710f2c7b146eb8639c4703bd0581ebfd
Windows-pat <imarcelo8000@gmail.com>
2023-03-13 18:25:26 +0100

    nesimo commit

--- practica1/script_experimentos.sh
+++ practica1/script_experimentos.sh
@@ -50,6 +50,3 @@ do
 	done
 done
 
-
-#formula de excel en español que busca el segundo numero más pequeños de una celda en varias páginas distintas las hojas se llaman datos $numero y la celda es F2:
-=MINIFS(F2;datos1!$A$2:$A$1000;datos1!$B$2:$B$1000;datos1!$C$2:$C$1000;datos1!$D$2:$D$1000;datos1!$E$2:$E$1000;datos1!$F$2:$F$1000;datos1!$G$2:$G$1000;datos1!$H$2:$H$1000;datos1!$I$2:$I$1000;datos1!$J$2:$J$1000;datos1!$K$2:$K$1000;datos1!$L$2:$L$1000;datos1!$M$2:$M$1000;datos1!$N$2:$N$1000;datos1!$O$2:$O$1000;datos1!$P$2:$P$1000;datos1!$Q$2:$Q$1000;datos1!$R$2:$R$1000;datos1!$S$2:$S$1000;datos1!$T$2:$T$1000;datos1!$U$2:$U$1000;datos1!$V$2:$V$1000;datos1!$W$2:$W$1000;datos1!$X$2:$X$1000;datos1!$Y$2:$Y$1000;datos1!$Z$2:$Z$1000;datos1!$AA$2:$AA$1000;datos1!$AB$2:$AB$1000;datos1!$AC$2:$AC$1000;datos1!$AD$2:$AD$1000;datos1!$AE$2:$AE$1000;datos1!$AF$2:$AF$1000;datos1!$AG$2:$AG$1000;datos1!$AH$2:$AH$1000;datos1!$AI$2:$AI$1000;datos1!$AJ$2:$AJ$1000;datos1!$AK$2:$AK$1000;datos1!$AL$2:$AL$1000;
