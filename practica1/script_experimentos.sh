11743942e5996b0b64707831af713268ed8d390b
Windows-pat <imarcelo8000@gmail.com>
2023-03-13 12:11:40 +0100

    mejoras en el script

--- practica1/script_experimentos.sh
+++ practica1/script_experimentos.sh
@@ -43,7 +43,7 @@ gcc acp1.c -o acp1 -msse3 -O0
 for i in {1..10}
 #for i in {1}
 do
-  #echo "############ Ejecución $i ############"
+  #echo "D:, L: , R: , ciclos: , accesos: , ciclos/acc:"
 	#for D in {$D1,$D2,$D3,$D4,$D5}
 	for D in {$D1,$D2,$D3,$D4,$D5,$D6,$D7,$D8,$D9,$D10,$D11,$D12,$D13,$D14}
 	do
