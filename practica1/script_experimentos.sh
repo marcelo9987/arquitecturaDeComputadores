ff57454c44b0d46c3b50c9cce11828537c44d296
Windows-pat <imarcelo8000@gmail.com>
2023-03-13 20:31:59 +0100

    cambio script para hacer pruebas

--- practica1/script_experimentos.sh
+++ practica1/script_experimentos.sh
@@ -1,18 +1,32 @@
 #!/bin/bash
 # Solicitamos un nodo con 64 cores y 256 GB de memoria durante 2 horas
-SBATCH -n 1 -c 64 -t 02:00:00 --mem=256G
+#SBATCH -n 1 -c 64 -t 02:00:00 --mem=256G
 # Ponemos nombre a nuestro trabajo para poder identificarlo.
 # ATENCIÓN - Debes sustituir el NN por el número de equipo.
 SBATCH --job-name p1acg03
 
 # Sustituir los valores de Di y Li por los calculados para la realización de la práctica.
 
+# D1=$((2 ** 0))
+# D2=$((2 ** 1))
+# D3=$((2 ** 2))
+# D4=$((2 ** 5))
+# D5=$((2 ** 8))
+
 D1=$((2 ** 0))
 D2=$((2 ** 1))
 D3=$((2 ** 2))
-D4=$((2 ** 5))
-D5=$((2 ** 8))
-
+D4=$((2 ** 3))
+D5=$((2 ** 4))
+D6=$((2 ** 5))
+D7=$((2 ** 6))
+D8=$((2 ** 7))
+D9=$((2 ** 8))
+D10=$((2 ** 9))
+D11=$((2 ** 10))
+D12=$((2 ** 11))
+D13=$((2 ** 12))
+D14=$((2 ** 13))
 
 # Para el portátil de Marcelo:
 #S1=$((32768/64))
@@ -40,7 +54,7 @@ do
   echo "D:, L:, R:, ciclos:, accesos:, ciclos/acc:" > resultados/datos.$i.csv
   #echo "D:, L: , R: , ciclos: , accesos: , ciclos/acc:"
 	#for D in {$D1,$D2,$D3,$D4,$D5}
-	for D in {$D1,$D2,$D3,$D4,$D5}
+	for D in {$D1,$D2,$D3,$D4,$D5,$D6,$D7,$D8,$D9,$D10,$D11,$D12,$D13,$D14}
 	do
 		for L in {$L1,$L2,$L3,$L4,$L5,$L6,$L7}
 		do
